b85d1bcaabb055296eaf3e327f550182d542a944
Hossein dinarvand <h.dinarvand@outlook.com>
2025-12-28 20:21:24 +0330

    feature : implement entities and relations between

---
+++ CommentsAndActivityLog/CommentsAndActivityLog.Application/Services/IActivityLogService.cs
@@ -0,0 +1,5 @@
+﻿namespace CommentsAndActivityLog.Application.Services;
+
+public interface IActivityLogService
+{
+}
---
+++ CommentsAndActivityLog/CommentsAndActivityLog.Domain/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace CommentsAndActivityLog.Domain.Entities;
+
+public class Comment
+{
+    public Comment()
+    {
+        ActivitiyLogList = new List<ActivityLog>();
+    }
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public int TaskId { get; set; }
+    public Task Task { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public List<ActivityLog> ActivitiyLogList { get; set; }
+}
