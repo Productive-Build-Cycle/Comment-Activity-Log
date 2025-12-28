988ddcc68daa9e8268ac18df93e2de1752113111
Hossein dinarvand <h.dinarvand@outlook.com>
2025-12-28 20:44:26 +0330

    feature : implement entities and project structure

---
+++ CommentActivityLog/CommentActivityLog.Domain/Entities/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace CommentsAndActivityLog.Domain.Entities;
+
+public class Task
+{
+    public Task()
+    {
+        CommentList = new List<Comment>();
+    }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public List<Comment> CommentList { get; set; }
+}
---
+++ CommentActivityLog/CommentActivityLog.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace CommentsAndActivityLog.Domain.Entities;
+
+public class User
+{
+    public User()
+    {
+        CommentList = new List<Comment>();
+    }
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public List<Comment> CommentList { get; set; }
+}
