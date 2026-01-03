3c61815fdf42271e47c6788c7724be9ec8b8e03c
Hossein dinarvand <h.dinarvand@outlook.com>
2026-01-03 22:04:26 +0330

    fix : edit activity log dto validatior

--- CommentActivityLog/CommentActivityLog.Application/Validations/ActivityLog/ActivityLogDtoValidation.cs
+++ CommentActivityLog/CommentActivityLog.Application/Validations/ActivityLog/ActivityLogDtoValidation.cs
@@ -7,24 +7,9 @@ public class ActivityLogDtoValidation : AbstractValidator<ActivityLogDto>
 {
     public ActivityLogDtoValidation()
     {
-        RuleFor(x => x.from_date)
-            .Must(BeAValidDate)
-            .When(x => x.from_date.HasValue)
-            .WithMessage("فرمت تاریخ شروع معتبر نیست");
-
-        RuleFor(x => x.to_date)
-            .Must(BeAValidDate)
-            .When(x => x.to_date.HasValue)
-            .WithMessage("فرمت تاریخ پایان معتبر نیست");
-
         RuleFor(x => x)
             .Must(x => x.from_date <= x.to_date)
             .When(x => x.from_date.HasValue && x.to_date.HasValue)
             .WithMessage("تاریخ شروع باید کوچکتر یا مساوی تاریخ پایان باشد");
-    }
-
-    private bool BeAValidDate(DateTime? date)
-    {
-        return date.HasValue;
     }
 }
